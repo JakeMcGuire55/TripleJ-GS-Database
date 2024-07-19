e026bc35cbf9fce540b45efba9240867bd5b5eb2
JakeMcGuire55 <jakemmcguire@gmail.com>
2024-07-18 23:44:08 -0400

    fixed server.js

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.DATABASE_URL || 3000;
+const cors = require('cors');
 require('dotenv').config();
 
 const applyRoutes = require('./routes');
